6457dcecd7c1e605a63c2ee41762c12e7a35e56d
97JUN <90228137+97JUN@users.noreply.github.com>
2024-08-21 17:57:02 +0900

    feat: 프로젝트 기본 세팅
    기본 세팅: PinLayout, FlexLayout, Alamofire 설치.

---
+++ BeTime/BeTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeTime
+//
+//  Created by 쭌이 on 8/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
