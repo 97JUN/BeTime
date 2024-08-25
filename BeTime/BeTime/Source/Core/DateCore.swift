d41e0ea3f64eac752ab323c67b796ee4f88e92c6
97JUN <90228137+97JUN@users.noreply.github.com>
2024-08-25 18:17:01 +0900

    DateCore, LocationCore 생성
    * DataCore 의 CurrentDate 형성 : 현재날짜, 시간을 담을 객체
    * LocationCore의 UserLocation 형성: 도시이름, 위도,경도 담을 객체

---
+++ BeTime/BeTime/Source/Core/DateCore.swift
@@ -0,0 +1,13 @@
+//
+//  DateCore.swift
+//  BeTime
+//
+//  Created by 쭌이 on 8/24/24.
+//
+
+import Foundation
+
+struct CurrentDate {
+  let date: String? // yyyyMMdd 형태
+  let time: String? // hhmm 형태
+}
---
+++ BeTime/BeTime/Source/Core/LocationCore.swift
@@ -0,0 +1,14 @@
+//
+//  LocationCore.swift
+//  BeTime
+//
+//  Created by 쭌이 on 8/23/24.
+//
+
+import Foundation
+
+struct UserLocation {
+  let cityName: String
+  let latitude: Int // 위도
+  let longitude: Int // 경도
+}
