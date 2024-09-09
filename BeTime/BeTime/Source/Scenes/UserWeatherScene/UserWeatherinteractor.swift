766366c504d8a67d6849d480e57097396d2222a3
97JUN <90228137+97JUN@users.noreply.github.com>
2024-09-09 18:40:04 +0900

    ViewModel 이름 변경 & 파일이름 변경( 안보셔도 됩니다!!!!)
    UserWeatherViewModel -> UserWeatherInteractor 로 변경

--- BeTime/BeTime/Source/Scenes/UserWeatherScene/UserWeatherinteractor.swift
+++ BeTime/BeTime/Source/Scenes/UserWeatherScene/UserWeatherinteractor.swift
@@ -1,5 +1,5 @@
 //
-//  UserWeatherViewModel.swift
+//  UserWeatherinteractor.swift
 //  BeTime
 //
 //  Created by 쭌이 on 8/27/24.
@@ -8,7 +8,7 @@
 import Foundation
 import RxSwift
 
-final class UserWeatherViewModel {
+final class UserWeatherinteractor {
   private let fetchWeatherUseCase: FetchWeatherUseCase
 
   private var skyConditionDatas: [WeatherForecast]?
