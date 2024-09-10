26a364143fc934bfe33e11d9f23c451c57724f4b
97JUN <90228137+97JUN@users.noreply.github.com>
2024-09-10 18:27:46 +0900

    UserWeatherTableViewCell 생성 & 파일 수정 ( 안보셔도 됩니다!!!!)

---
+++ BeTime/BeTime/Source/UserWeatherScene/UserWeatherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserWeatherTableViewCell.swift
+//  BeTime
+//
+//  Created by 쭌이 on 9/10/24.
+//
+
+import UIKit
+
+class UserWeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
