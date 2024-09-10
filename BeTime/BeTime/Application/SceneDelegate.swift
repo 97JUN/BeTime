d4315dc3080a9e3ab3e58de884a1c64f55974b3d
97JUN <90228137+97JUN@users.noreply.github.com>
2024-09-10 14:29:53 +0900

    SceneDelegate 개행수정 (안보셔도 됩니다!!)

--- BeTime/BeTime/Application/SceneDelegate.swift
+++ BeTime/BeTime/Application/SceneDelegate.swift
@@ -8,35 +8,34 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
 
-    var window: UIWindow?
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+    let window = UIWindow(windowScene: windowScene)
+    window.rootViewController = CityListViewController()
+    self.window = window
+    window.makeKeyAndVisible()
+  }
 
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = CityListViewController()
-        self.window = window
-        window.makeKeyAndVisible()
-    }
+  func sceneDidDisconnect(_ scene: UIScene) {
+    // Scene이 시스템에 의해 종료될 때 호출됩니다.
+  }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Scene이 시스템에 의해 종료될 때 호출됩니다.
-    }
+  func sceneDidBecomeActive(_ scene: UIScene) {
+    // Scene이 활성화될 때 호출됩니다.
+  }
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Scene이 활성화될 때 호출됩니다.
-    }
+  func sceneWillResignActive(_ scene: UIScene) {
+    // Scene이 비활성화될 때 호출됩니다.
+  }
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Scene이 비활성화될 때 호출됩니다.
-    }
+  func sceneWillEnterForeground(_ scene: UIScene) {
+    // Scene이 포그라운드로 진입할 때 호출됩니다.
+  }
 
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Scene이 포그라운드로 진입할 때 호출됩니다.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Scene이 백그라운드로 전환될 때 호출됩니다.
-    }
+  func sceneDidEnterBackground(_ scene: UIScene) {
+    // Scene이 백그라운드로 전환될 때 호출됩니다.
+  }
 }
