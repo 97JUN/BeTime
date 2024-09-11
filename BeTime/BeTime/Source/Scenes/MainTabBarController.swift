d2ae7e39e3b5dec198c7f4e4c47e22e4bf07a26a
97JUN <90228137+97JUN@users.noreply.github.com>
2024-09-11 21:02:42 +0900

    CityListViewController & MainTabBarController 추가

---
+++ BeTime/BeTime/Source/Scenes/MainTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  MainTabBarController.swift
+//  BeTime
+//
+//  Created by 쭌이 on 9/11/24.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
