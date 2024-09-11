909ccccef89ce22a753052bc087fe489b2fd3898
97JUN <90228137+97JUN@users.noreply.github.com>
2024-09-11 20:48:10 +0900

    UserWeatherSceneDIContainer 추가 & 의존성 주입 구현

---
+++ BeTime/BeTime/Source/Application/DIContainers/UserWeatherSceneDIContainer.swift
@@ -0,0 +1,39 @@
+//
+//  UserWeatherSceneDIContainer.swift
+//  BeTime
+//
+//  Created by 쭌이 on 9/11/24.
+//
+
+import Foundation
+import Swinject
+
+final class UserWeatherSceneDIContainer {
+  let container: Container
+
+  init() {
+    container = Container()
+
+    container.register(WeatherDataRepository.self) { _ in
+      WeatherDataRepository()
+    }
+
+    container.register(FetchWeatherUseCase.self) { resolver in
+      FetchWeatherUseCaseImpl(weatherRepository: resolver.resolve(WeatherDataRepository.self)!)
+    }
+
+    container.register(UserWeatherInteractor.self) { resolver in
+      UserWeatherInteractor(searchWeatherUseCase: resolver.resolve(FetchWeatherUseCase.self)!)
+    }
+
+    container.register(UserWeatherViewController.self) { resolver in
+      let viewController = UserWeatherViewController()
+      viewController.interactor = resolver.resolve(UserWeatherInteractor.self)
+      return viewController
+    }
+  }
+
+  func resolveUserWeatherViewController() -> UserWeatherViewController {
+    return container.resolve(UserWeatherViewController.self)!
+  }
+}
