a1e19b7d663c2fe083991dfaa95539d69f0b8589
97JUN <90228137+97JUN@users.noreply.github.com>
2024-09-11 20:28:10 +0900

    CityListViewController & UserWeatherTableViewCell 추가

---
+++ BeTime/BeTime/Source/Scenes/CityListScene/CityListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CityListViewController.swift
+//  BeTime
+//
+//  Created by 쭌이 on 9/11/24.
+//
+
+import UIKit
+
+final class CityListViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
---
+++ BeTime/BeTime/Source/Scenes/UserWeatherScene/UserWeatherTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserWeatherTableViewCell.swift
+//  BeTime
+//
+//  Created by 쭌이 on 9/11/24.
+//
+
+import UIKit
+
+final class UserWeatherTableViewCell: UITableViewCell {
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+}
