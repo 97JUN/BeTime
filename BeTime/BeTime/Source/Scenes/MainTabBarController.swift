47168d3f046e32254709963a02092a1d435cd1e1
97JUN <90228137+97JUN@users.noreply.github.com>
2024-09-12 02:12:24 +0900

    MainTabBarController 구성

--- BeTime/BeTime/Source/Scenes/MainTabBarController.swift
+++ BeTime/BeTime/Source/Scenes/MainTabBarController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 final class MainTabBarController: UITabBarController {
+  var userWeatherSceneDIContainer: UserWeatherSceneDIContainer!
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.setTabBar()
+  }
+
+  private func setTabBar() {
+    userWeatherSceneDIContainer = UserWeatherSceneDIContainer()
+    let userWeatherViewController = userWeatherSceneDIContainer.resolveUserWeatherViewController()
+
+    let firstViewController = UINavigationController(rootViewController: userWeatherViewController)
+    firstViewController.tabBarItem = UITabBarItem(title: "MyWeather", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
+
+    let secondViewController = UINavigationController(rootViewController: CityListViewController())
+    secondViewController.tabBarItem = UITabBarItem(title: "CityList", image: UIImage(systemName: "list.bullet"), selectedImage: UIImage(systemName: "list.bullet"))
+
+    self.viewControllers = [firstViewController, secondViewController]
+    self.tabBar.backgroundColor = .lightGray
   }
 }
