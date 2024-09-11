f4016f7714c99717204f2f65dae82c90db2e06c6
97JUN <90228137+97JUN@users.noreply.github.com>
2024-09-11 20:00:40 +0900

    UserWeatherSceneDIContainer에서 의존성 주입

--- BeTime/BeTime/Source/Application/DIContainers/UserWeatherSceneDIContainer.swift
+++ BeTime/BeTime/Source/Application/DIContainers/UserWeatherSceneDIContainer.swift
@@ -6,7 +6,35 @@
 //
 
 import Foundation
+import Swinject
+import UIKit
 
 final class UserWeatherSceneDIContainer {
+  let container: Container
 
+  init() {
+    container = Container()
+
+    container.register(WeatherDataRepository.self) { _ in
+      WeatherDataRepository()
+    }
+
+    container.register(FetchWeatherUseCase.self) { resolver in
+      FetchWeatherUseCaseImpl(weatherRepository: resolver.resolve(WeatherDataRepository.self)!)
+    }
+
+    container.register(UserWeatherInteractor.self) { resolver in
+      UserWeatherInteractor(searchWeatherUseCase: resolver.resolve(FetchWeatherUseCase.self)!)
+    }
+
+    container.register(UserWeatherViewController.self) { resolver in
+      let viewController = UserWeatherViewController()
+      viewController.interactor = resolver.resolve(UserWeatherInteractor.self)
+      return viewController
+    }
+  }
+
+  func resolveUserWeatherViewController() -> UserWeatherViewController {
+    return container.resolve(UserWeatherViewController.self)!
+  }
 }
