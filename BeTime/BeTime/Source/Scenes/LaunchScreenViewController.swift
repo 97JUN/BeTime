24e999aeff39eafae648413bf196f67764e9d904
97JUN <90228137+97JUN@users.noreply.github.com>
2024-09-13 14:59:01 +0900

    LaunchScreen 코드 추가

---
+++ BeTime/BeTime/Source/Scenes/LaunchScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenViewController.swift
+//  BeTime
+//
+//  Created by 쭌이 on 9/13/24.
+//
+
+import UIKit
+
+final class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
