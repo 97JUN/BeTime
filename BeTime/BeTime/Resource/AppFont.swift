7b09d08721854202b29ad42c0f3c976d2b971f68
97JUN <90228137+97JUN@users.noreply.github.com>
2024-09-13 19:31:15 +0900

    AppFont 파일 생성, 폰트 추가 & Project 설정 iOS15 로 설정
    iOS 15로 수정(for addAction), AppFont 파일 추가
    
    AppFont 에 app에서 사용하는 Font 추가

---
+++ BeTime/BeTime/Resource/AppFont.swift
@@ -0,0 +1,24 @@
+//
+//  AppFont.swift
+//  BeTime
+//
+//  Created by 쭌이 on 9/13/24.
+//
+
+import UIKit
+
+extension UIFont {
+
+  static let appTitleFont: UIFont = {
+    return UIFont.systemFont(ofSize: 25)
+  }()
+
+  static let appSubTitleFont: UIFont = {
+    return UIFont.systemFont(ofSize: 20)
+  }()
+
+  static let appValueFont: UIFont = {
+    return UIFont.systemFont(ofSize: 15)
+  }()
+
+}
