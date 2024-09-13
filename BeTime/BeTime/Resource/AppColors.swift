2aef30fecac52cfcd742b89d0a367c405d289dd0
97JUN <90228137+97JUN@users.noreply.github.com>
2024-09-13 15:19:35 +0900

    AppColor 추가 & background Color 추가

---
+++ BeTime/BeTime/Resource/AppColors.swift
@@ -0,0 +1,16 @@
+//
+//  AppColors.swift
+//  BeTime
+//
+//  Created by 쭌이 on 9/13/24.
+//
+
+import UIKit
+
+extension UIColor {
+
+  static let appBackgroundColor: UIColor = {
+    return UIColor(red: 160/255, green: 194/255, blue: 204/255, alpha: 1.0)
+  }()
+
+}
